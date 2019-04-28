8fb0a2b76cb28d4bab33562ef82f7b16617e655d
Andrei Fangli <andrei_fangli@outlook.com>
2019-04-28 20:19:59 +0300

    Added assembly reference & member reference factory under Reference Data

---
+++ CodeMap.Tests/MemberReferenceFactoryTests.cs
@@ -0,0 +1,49 @@
+﻿using CodeMap.ReferenceData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodeMap.Tests
+{
+    public class MemberReferenceFactoryTests
+    {
+        private MemberReferenceFactory _Factory { get; } = new MemberReferenceFactory();
+
+        [Fact]
+        public void CreateFromAssembly_ReturnsAssemblyReference()
+        {
+            var assemblyReference = _Factory.Create(typeof(GlobalTestClass).Assembly);
+
+            Assert.Equal("CodeMap.Tests.Data", assemblyReference.Name);
+            Assert.Equal(new Version(1, 2, 3, 4), assemblyReference.Version);
+            Assert.Empty(assemblyReference.Culture);
+            Assert.Empty(assemblyReference.PublicKeyToken);
+        }
+
+        [Fact]
+        public void CreateFromAssemblyName_ReturnsAssemblyReference()
+        {
+            var assemblyReference = _Factory.Create(typeof(GlobalTestClass).Assembly.GetName());
+
+            Assert.Equal("CodeMap.Tests.Data", assemblyReference.Name);
+            Assert.Equal(new Version(1, 2, 3, 4), assemblyReference.Version);
+            Assert.Empty(assemblyReference.Culture);
+            Assert.Empty(assemblyReference.PublicKeyToken);
+        }
+
+        [Fact]
+        public void CreateFromNullAssembly_ThrowsException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>("assembly", () => _Factory.Create(assembly: null));
+            Assert.Equal(new ArgumentNullException("assembly").Message, exception.Message);
+        }
+
+        [Fact]
+        public void CreateFromNullAssemblyName_ThrowsException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>("assemblyName", () => _Factory.Create(assemblyName: null));
+            Assert.Equal(new ArgumentNullException("assemblyName").Message, exception.Message);
+        }
+    }
+}
---
+++ CodeMap/ReferenceData/MemberReferenceFactory.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CodeMap.ReferenceData
+{
+    /// <summary>Represents a <see cref="MemberReference"/> factory.</summary>
+    public class MemberReferenceFactory
+    {
+        private readonly IDictionary<AssemblyName, AssemblyReference> _cachedAssemblyReferences = new Dictionary<AssemblyName, AssemblyReference>(new AssemblyNameEqualityComparer());
+        private readonly IDictionary<MemberInfo, MemberReference> _cachedMemberReferences = new Dictionary<MemberInfo, MemberReference>();
+
+        /// <summary>Initializes a new instance of the <see cref="MemberReferenceFactory"/> class.</summary>
+        public MemberReferenceFactory()
+        {
+            _cachedAssemblyReferences = new Dictionary<AssemblyName, AssemblyReference>(new AssemblyNameEqualityComparer());
+        }
+
+        /// <summary>Creates an <see cref="AssemblyReference"/> for the provided <paramref name="assembly"/>.</summary>
+        /// <param name="assembly">The <see cref="Assembly"/> for which to create the reference.</param>
+        /// <returns>Returns an <see cref="AssemblyReference"/> for the provided <paramref name="assembly"/>.</returns>
+        public AssemblyReference Create(Assembly assembly)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException(nameof(assembly));
+
+            return Create(assembly.GetName());
+        }
+
+        /// <summary>Creates an <see cref="AssemblyReference"/> for the provided <paramref name="assemblyName"/>.</summary>
+        /// <param name="assemblyName">The <see cref="AssemblyName"/> for which to create the reference.</param>
+        /// <returns>Returns an <see cref="AssemblyReference"/> for the provided <paramref name="assemblyName"/>.</returns>
+        public AssemblyReference Create(AssemblyName assemblyName)
+        {
+            if (assemblyName == null)
+                throw new ArgumentNullException(nameof(assemblyName));
+
+            if (!_cachedAssemblyReferences.TryGetValue(assemblyName, out var assemblyReference))
+            {
+                assemblyReference = _GetAssemblyReference(assemblyName);
+                _cachedAssemblyReferences.Add(assemblyName, assemblyReference);
+            }
+            return assemblyReference;
+        }
+
+        private static AssemblyReference _GetAssemblyReference(AssemblyName assemblyName)
+            => new AssemblyReference
+            {
+                Name = assemblyName.Name,
+                Version = assemblyName.Version,
+                Culture = assemblyName.CultureName,
+                PublicKeyToken = assemblyName.GetPublicKeyToken().ToBase16String()
+            };
+
+        private sealed class AssemblyNameEqualityComparer : IEqualityComparer<AssemblyName>
+        {
+            public bool Equals(AssemblyName left, AssemblyName right)
+            {
+                if (left == null)
+                    return right == null;
+
+                return right != null
+                    && AssemblyName.ReferenceMatchesDefinition(left, right)
+                    && string.Equals(left.Name, right.Name, StringComparison.OrdinalIgnoreCase)
+                    && left.Version == right.Version
+                    && string.Equals(left.CultureName, right.CultureName, StringComparison.OrdinalIgnoreCase)
+                    && string.Equals(left.GetPublicKeyToken().ToBase16String(), right.GetPublicKeyToken().ToBase16String(), StringComparison.OrdinalIgnoreCase);
+            }
+
+            public int GetHashCode(AssemblyName asseblyName)
+                => asseblyName != null
+                    ? new
+                    {
+                        Name = asseblyName.Name?.ToLowerInvariant(),
+                        asseblyName.Version,
+                        Culture = asseblyName.CultureName?.ToLowerInvariant(),
+                        PublicKeyToken = asseblyName.GetPublicKeyToken().ToBase16String()
+                    }.GetHashCode()
+                    : throw new ArgumentNullException(nameof(asseblyName));
+        }
+    }
+}
